4e565d41b325c5e3d2acf57ae026257bfaa44c10
benjicoopz <benjicoopss@gmail.com>
2023-12-19 15:58:20 -0600

    add choice A

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from models import Base, session, Product, engine
 import datetime
 import csv
+import time
 
 
 def menu():
@@ -24,41 +25,69 @@ def menu():
 
 
 def clean_date(date_str):
-    months = ['January', 'February', 'March', 'April', 'May', 'June',
-              'July', 'August', 'September', 'October',
-              'November', 'December']
-    split_date = date_str.split(' ')
-    month = int(months.index(split_date[0]) + 1)
-    day = int(split_date[1].split(',')[0])
-    year = int(split_date[2])
-    return datetime.date(year, month, day)
+    split_date = date_str.split('/')
+    try:
+        month = int(split_date[0])
+        day = int(split_date[1])
+        year = int(split_date[2])
+        return_date = datetime.date(year, month, day)
+    except Exception:
+        input('''
+        \n***INVALID FORMAT***
+        \rDate Format Should be: MM/DD/YYYY
+        \rIf Single Digit for MM/DD, NO NEED FOR 0
+        \rEx: 3/7/2018
+        \rPress Enter to Try Again
+        \r*********************''')
+        return
+    else:
+        return return_date
 
 
 def clean_price(price_str):
-    split_price = price_str.split('$')
-    if len(split_price) < 2:
-        return None
-    nosignprice = split_price[1]
-    price_float = float(nosignprice)
-    return int(price_float * 100)
+    try:
+        split_price = price_str.split('$')
+        nosignprice = split_price[1]
+        price_float = float(nosignprice)
+    except Exception:
+        input('''
+            \n***INVALID FORMAT***
+            \rPrice Format Should be: $21.05
+            \rBe Sure to Include the Dollar Sign!
+            \rPress Enter to Try Again
+            \r*********************''')
+    else:
+        return int(price_float * 100)
 
 
 def clean_quan(quan_str):
-    quan_int = int(quan_str)
-    return quan_int
+    try:
+        quan_int = int(quan_str)
+    except Exception:
+        input('''
+        \n***INVALID ENTRY***
+        \rProduct Quantity Should be a Whole Number
+        \rEx: 87
+        \rPress Enter to Try Again
+        \r*********************''')
+    else:
+        return quan_int
 
 
 def add_csv():
     with open('inventory.csv') as csvfile:
         data = csv.reader(csvfile)
+        next(data)
         for row in data:
-            print(row)
-            product = row[0]
-            price = clean_price(row[1])
-            quantity = clean_quan(row[2])
-            date = clean_date(row[3])
-            new_prod = Product(product_name=product, product_price=price, product_quantity=quantity, date_updated=date)
-            session.add(new_prod)
+            product_in_db = session.query(Product).filter(Product.product_name == row[0]).one_or_none()
+            if product_in_db is None:
+                product = row[0]
+                price = clean_price(row[1])
+                quantity = clean_quan(row[2])
+                date = clean_date(row[3])
+                new_prod = Product(product_name=product, product_price=price,
+                                   product_quantity=quantity, date_updated=date)
+                session.add(new_prod)
         session.commit()
 
 
@@ -69,7 +98,31 @@ def app():
         if choice == 'v':
             pass
         elif choice == 'a':
-            pass
+            name = input('Product Name: ')
+            amount_error = True
+            while amount_error:
+                amount = input('Product Quantity: ')
+                amount_clean = clean_quan(amount)
+                if type(amount_clean) == int:
+                    amount_error = False
+            price_error = True
+            while price_error:
+                price = input('Product Price (Ex:$5.70): ')
+                price_clean = clean_price(price)
+                if type(price_clean) == int:
+                    price_error = False
+            date_error = True
+            while date_error:
+                date = input('Date Updated (Ex: MM/DD/YYYY): ')
+                date_clean = clean_date(date)
+                if type(date_clean) == datetime.date:
+                    date_error = False
+            new_product = Product(product_name=name, product_quantity=amount_clean,
+                                  product_price=price_clean, date_updated=date_clean)
+            session.add(new_product)
+            session.commit()
+            print('Product Added!')
+            time.sleep(2)
         elif choice == 'b':
             pass
         else:
@@ -79,8 +132,11 @@ def app():
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
-    # app()
     add_csv()
+    app()
 
     for prod in session.query(Product):
         print(prod)
+
+
+
